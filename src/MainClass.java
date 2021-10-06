63b7490a6a55204327c0669a2b2ed3338ecd1586
Eugene <gef.ossystem@gmail.com>
2021-10-06 22:46:32 +0300

    skip

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,9 +1,5 @@
 public class MainClass {
 
-    public String class_string = "Hello, world";
 
-    public String getClassString() {
-        return class_string;
-    }
 
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -8,10 +8,7 @@ public class MainClassTest {
 
     @Test
     public void testGetClassString() {
-        String expected_string = "hello";
 
-        Assert.assertTrue("ClassString doesn't contains Hello or hello substring"
-                , mainClass.getClassString().toLowerCase().contains(expected_string.toLowerCase()));
     }
 
 }
