b32c4c8001b10f94455c651432b37aee309496d4
Eugene <gef.ossystem@gmail.com>
2021-10-06 22:49:04 +0300

    test 3 --> Add new test 'testGetClassString'

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,5 +1,9 @@
 public class MainClass {
 
+    public String class_string = "Hello, world";
 
+    public String getClassString() {
+        return class_string;
+    }
 
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,14 +1,16 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Locale;
 
 public class MainClassTest {
     MainClass mainClass = new MainClass();
 
     @Test
     public void testGetClassString() {
+        String expected_string = "hello";
 
+        Assert.assertTrue("ClassString doesn't contains Hello or hello substring"
+                , mainClass.getClassString().toLowerCase().contains(expected_string.toLowerCase()));
     }
 
 }
