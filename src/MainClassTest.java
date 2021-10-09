74059f01332421b0ebf0887afc6fb605d483b752
Eugene <gef.ossystem@gmail.com>
2021-10-09 17:56:20 +0300

    test 3 -> Add regex in testGetClassString

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,14 +1,18 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.regex.Pattern;
+
 
 public class MainClassTest {
     MainClass mainClass = new MainClass();
 
     @Test
     public void testGetClassString() {
+        Pattern MY_PATTERN = Pattern.compile("\\b^[hH]ello\\b");
+
         Assert.assertTrue("ClassString doesn't contains Hello or hello substring"
-                , (mainClass.getClassString().contains("hello") || mainClass.getClassString().contains("Hello")));
+                ,         MY_PATTERN.matcher(mainClass.getClassString()).find());
     }
 
 }
